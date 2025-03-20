db9d8b36812ff7ecfc974c8884e7680b4f831ad5
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-03-20 18:22:34 +0100

    Fix tests

--- sparse_strips/vello_cpu/src/fine.rs
+++ sparse_strips/vello_cpu/src/fine.rs
@@ -69,7 +69,7 @@ impl<'a> Fine<'a> {
         );
     }
 
-    pub(crate) fn run_cmd(&mut self, cmd: &Cmd, alphas: &[u8]) {
+    pub(crate) fn run_cmd(&mut self, tile_x: u16, cmd: &Cmd, alphas: &[u8]) {
         match cmd {
             Cmd::Fill(f) => {
                 self.fill(f.x as usize, tile_x, f.width as usize, &f.paint);
@@ -194,10 +194,13 @@ pub(crate) mod strip {
         mut color_iter: T,
         alphas: &[u8],
     ) {
-        for (bg_c, masks) in target.chunks_exact_mut(TILE_HEIGHT_COMPONENTS).zip(alphas) {
+        for (bg_c, masks) in target
+            .chunks_exact_mut(TILE_HEIGHT_COMPONENTS)
+            .zip(alphas.chunks_exact(usize::from(Tile::HEIGHT)))
+        {
             for j in 0..usize::from(Tile::HEIGHT) {
                 let src_c = color_iter.next().unwrap();
-                let mask_a = ((*masks >> (j * 8)) & 0xff) as u16;
+                let mask_a = u16::from(masks[j]);
                 let inv_src_a_mask_a = 255 - div_255(mask_a * src_c[3] as u16);
 
                 for i in 0..COLOR_COMPONENTS {
