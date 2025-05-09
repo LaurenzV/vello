cea30dd7453b535c11c3711ebc9494b3d00ae48f
Laurenz Stampfl <laurenz.stampfl+github@gmail.com>
2025-05-09 11:53:37 +0200

    more

--- sparse_strips/vello_bench/src/main.rs
+++ sparse_strips/vello_bench/src/main.rs
@@ -6,10 +6,12 @@ use vello_common::paint::{Paint, PremulColor};
 use vello_common::peniko::{BlendMode, Compose, Mix};
 use vello_cpu::fine2;
 use vello_cpu::fine2::{SCRATCH_BUF_SIZE};
-use vello_cpu::fine::ScratchBuf;
+use vello_cpu::fine::{Fine, ScratchBuf};
 
 fn main() {
-    let paint = PremulColor::from_alpha_color(ROYAL_BLUE).as_premul_rgba8().to_u8_array();
+    let paint = Paint::Solid(PremulColor::from_alpha_color(ROYAL_BLUE));
+    let paints: Vec<EncodedPaint> = vec![];
+    let u8_arr = PremulColor::from_alpha_color(ROYAL_BLUE).as_premul_rgba8().to_u8_array();
     let blend_mode = BlendMode::new(Mix::Normal, Compose::SrcOver);
     let mut blend_buf = [0u8; SCRATCH_BUF_SIZE];
     
@@ -18,13 +20,14 @@ fn main() {
     let mut count = 0;
     let iter_count = 100;
     let mut duration = Duration::default();
-    // let mut fine = Fine::<f32>::new(256, 4);
+    let mut fine = Fine::<u8>::new(256, 4);
 
     while duration.as_millis() < max_duration {
         let start = Instant::now();
         
         for _ in 0..100 {
-            fine2::opaque(&mut blend_buf, &paint);
+            fine.fill(0, 256, &paint, blend_mode, &paints);
+            // fine2::opaque(&mut blend_buf, &u8_arr);
         }
         
         std::hint::black_box(&blend_buf);
@@ -32,8 +35,6 @@ fn main() {
         count += 1;
         duration += start.elapsed();
     }
-    
-    eprintln!("{:?}", duration);
     
     let duration = duration.as_secs_f64() / (count * iter_count) as f64;
     
--- sparse_strips/vello_cpu/src/fine2/mod.rs
+++ sparse_strips/vello_cpu/src/fine2/mod.rs
@@ -1,3 +1,6 @@
+use crate::fine::ScratchBuf;
+use crate::Paint;
+
 pub const HEIGHT: usize = 4;
 pub const WIDETILE_WIDTH: usize = 256;
 const COLOR_COMPONENTS: usize = 4;
@@ -10,9 +13,66 @@ pub fn opaque(
     blend_buf: &mut [u8],
     color: &[u8; 4]
 ) {
-    let blend_buf = &mut blend_buf[..SCRATCH_BUF_SIZE];
+    let blend_buf = &mut blend_buf[0..][..1024];
+
     
     for t in blend_buf.chunks_exact_mut(COLOR_COMPONENTS) {
         t.copy_from_slice(color);
+    }
+}
+
+pub struct Fine {
+    pub(crate) width: u16,
+    pub(crate) height: u16,
+    pub(crate) wide_coords: (u16, u16),
+    pub(crate) blend_buf: Vec<ScratchBuf<u8>>,
+    pub(crate) color_buf: ScratchBuf<u8>,
+}
+
+impl Fine {
+    /// Create a new fine rasterizer.
+    pub fn new(width: u16, height: u16) -> Self {
+        let blend_buf = [0; SCRATCH_BUF_SIZE];
+        let color_buf = [0; SCRATCH_BUF_SIZE];
+
+        Self {
+            width,
+            height,
+            wide_coords: (0, 0),
+            blend_buf: vec![blend_buf],
+            color_buf,
+        }
+    }
+
+    /// Fill at a given x and with a width using the given paint.
+    pub fn fill(
+        &mut self,
+        x: usize,
+        width: usize,
+        fill: &Paint,
+    ) {
+        let blend_buf = &mut self.blend_buf.last_mut().unwrap()[x * TILE_HEIGHT_COMPONENTS..]
+            [..TILE_HEIGHT_COMPONENTS * width];
+        let color_buf =
+            &mut self.color_buf[x * TILE_HEIGHT_COMPONENTS..][..TILE_HEIGHT_COMPONENTS * width];
+
+        let start_x = self.wide_coords.0 * WIDETILE_WIDTH as u16 + x as u16;
+        let start_y = self.wide_coords.1 * HEIGHT as u16;
+
+        match fill {
+            Paint::Solid(color) => {
+                let color = color.as_premul_rgba8().to_u8_array();
+
+                // If color is completely opaque we can just memcopy the colors.
+                if color[3] == 255  {
+                    for t in blend_buf.chunks_exact_mut(COLOR_COMPONENTS) {
+                        t.copy_from_slice(&color);
+                    }
+
+                    return;
+                }
+            }
+            _ => unreachable!()
+        }
     }
 }
