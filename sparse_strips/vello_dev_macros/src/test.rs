86a7c6e1434e1e0bb844acaedd093302ecdffc3f
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2025-05-12 10:20:02 +0200

    Disable clipping tests for now

--- sparse_strips/vello_dev_macros/src/test.rs
+++ sparse_strips/vello_dev_macros/src/test.rs
@@ -100,6 +100,7 @@ pub(crate) fn vello_test_inner(attr: TokenStream, item: TokenStream) -> TokenStr
 
     skip_cpu |= {
         input_fn_name_str.contains("compose")
+            || input_fn_name_str.contains("clip")
             || input_fn_name_str.contains("gradient")
             || input_fn_name_str.contains("colr")
             || input_fn_name_str.contains("bitmap")
