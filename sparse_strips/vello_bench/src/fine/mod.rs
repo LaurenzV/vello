46cc2253ad41ba34f353f84fd92e0e191d28db3b
Laurenz Stampfl <47084093+LaurenzV@users.noreply.github.com>
2025-05-15 16:23:32 +0200

    Re-add benchmarks

--- sparse_strips/vello_bench/src/fine/mod.rs
+++ sparse_strips/vello_bench/src/fine/mod.rs
@@ -1,4 +1,5 @@
 mod blend;
+mod clear;
 pub(crate) mod fill;
 mod gradient;
 mod image;
@@ -6,27 +7,29 @@ mod rounded_blurred_rect;
 mod strip;
 
 use criterion::Bencher;
-use vello_cpu::fine::{Fine, FineType, SCRATCH_BUF_SIZE};
+use vello_cpu::fine2::{Fine, SCRATCH_BUF_SIZE};
 use vello_dev_macros::vello_bench;
 
 pub use blend::*;
+pub use clear::*;
 pub use fill::*;
 pub use gradient::*;
 pub use image::*;
 pub use rounded_blurred_rect::*;
 pub use strip::*;
 use vello_common::peniko::{BlendMode, Compose, Mix};
+use vello_simd::Type;
 
-// #[vello_bench]
-// pub fn pack<F: FineType>(b: &mut Bencher<'_>, fine: &mut Fine<F>) {
-//     let mut buf = vec![0; SCRATCH_BUF_SIZE];
-//
-//     b.iter(|| {
-//         fine.pack(&mut buf);
-//         std::hint::black_box(&buf);
-//     });
-// }
-//
+#[vello_bench]
+pub fn pack<F: Type>(b: &mut Bencher<'_>, fine: &mut Fine<F>) {
+    let mut buf = vec![0; SCRATCH_BUF_SIZE];
+
+    b.iter(|| {
+        fine.pack(&mut buf);
+        std::hint::black_box(&buf);
+    });
+}
+
 pub(crate) fn default_blend() -> BlendMode {
     BlendMode::new(Mix::Normal, Compose::SrcOver)
 }
